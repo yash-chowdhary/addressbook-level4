a83f89f730328bf2a1d8c2eb93280a604bf6be12
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-08 22:57:01 +0800

    Add RemoveGroupCommand class and relevant Exception classes

---
+++ src/main/java/seedu/address/model/person/exceptions/GroupNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.person.exceptions;
+
+/**
+ * Signals that the operation is unable to find the required Group
+ */
+public class GroupNotFoundException extends Exception {
+}
