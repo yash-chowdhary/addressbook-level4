bc59385e1af2826083a0a686320a97263fd8a495
Song Weiyang <e0014576@u.nus.edu>
2018-03-13 23:15:19 +0800

    Add username class

---
+++ src/main/java/seedu/address/model/person/Username.java
@@ -0,0 +1,17 @@
+package seedu.address.model.person;
+
+/**
+ * Represent a member's username
+ */
+public class Username {
+    public final String value;
+
+    public Username(String name){
+        this.value = name;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
