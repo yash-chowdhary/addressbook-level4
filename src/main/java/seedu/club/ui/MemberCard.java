9db4e410cf5769d1727664221ac47de0a43085a9
amrut-prabhu <amrut08@gmail.com>
2018-03-16 11:55:52 +0800

    More refactoring

--- src/main/java/seedu/club/ui/MemberCard.java
+++ src/main/java/seedu/club/ui/MemberCard.java
@@ -12,14 +12,14 @@ import seedu.club.model.member.Member;
  */
 public class MemberCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "MemberListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
      * or an exception will be thrown by JavaFX during runtime.
      *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on ClubBook level 4</a>
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
     public final Member member;
