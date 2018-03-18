a98201cab87927edc87158b35aa6cfb1e9e3558c
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-18 17:20:42 +0800

    Body, Client, and Subject tests

---
+++ src/test/java/seedu/club/logic/commands/email/SubjectTest.java
@@ -0,0 +1,27 @@
+package seedu.club.logic.commands.email;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SubjectTest {
+
+    @Test
+    public void equals() {
+        Subject firstSubject = new Subject(Subject.TEST_SUBJECT_STRING);
+        Subject secondSubject = new Subject(Subject.TEST_SUBJECT_STRING);
+
+        assertTrue(firstSubject.equals(firstSubject));
+        assertTrue(firstSubject.equals(secondSubject));
+
+        assertFalse(firstSubject.equals(null));
+    }
+
+    @Test
+    public void test_hashCode() {
+        Subject subject = new Subject(Subject.EMPTY_SUBJECT_STRING);
+        assertEquals(subject.hashCode(), Subject.EMPTY_SUBJECT_STRING.hashCode());
+    }
+}
