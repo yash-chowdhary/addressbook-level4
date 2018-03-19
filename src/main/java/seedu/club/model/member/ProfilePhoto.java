35a516a994e3d3368fe141ca6b37390b27362a60
amrut-prabhu <amrut08@gmail.com>
2018-03-19 21:58:24 +0800

    Add ProfilePhoto class

--- src/main/java/seedu/club/model/member/ProfilePhoto.java
+++ src/main/java/seedu/club/model/member/ProfilePhoto.java
@@ -3,18 +3,19 @@ package seedu.club.model.member;
 import static java.util.Objects.requireNonNull;
 import static seedu.club.commons.util.AppUtil.checkArgument;
 
+//@author amrut-prabhu
 /**
  * Represents a member's profile photo in the club book.
  */
 public class ProfilePhoto {
     public static final String MESSAGE_PHOTO_PATH_CONSTRAINTS =
-            "the photo path should follow the format of this example: C://Downloads//mypic.png";
+            "the photo path should follow the format of this example: C://Downloads//...//mypic.png";
 
     /*
      * The first character of the club must be a single alphabet. It is followed by ":", then "\\",
      * a directory whose name consists of alphabets and/or digits, followed by a "." and the file type.
      */
-    public static final String IMAGE_PATH_VALIDATION_REGEX = "(.*/)*.+\\\\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP)$";
+    public static final String IMAGE_PATH_VALIDATION_REGEX = "(.*/)*.+\\\\.(png|jpg|jpeg|PNG|JPG)$";
     //https://stackoverflow.com/questions/7102890/java-regex-for-image-filename
     //"([a-zA-Z]:)?(\\\\[a-zA-Z0-9_.-]+)+\\\\?";  https://stackoverflow.com/questions/4489582/java-regular-expression-to-match-file-path
     //([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)  http://www.mkyong.com/regular-expressions/how-to-validate-image-file-extension-with-regular-expression/
@@ -34,6 +35,8 @@ public class ProfilePhoto {
 
     /**
      * Returns true if a given string is a valid photo path.
+     *
+     * @param test Path whose validity is to be checked.
      */
     public static boolean isValidPhotoPath(String test) {
         return test.matches(IMAGE_PATH_VALIDATION_REGEX);
@@ -41,7 +44,8 @@ public class ProfilePhoto {
 
     /**
      * Sets the {@code photoFilePath} to the specified {@code path}.
-     * @param path A valid image path
+     *
+     * @param path A valid image path.
      */
     //TODO: make valid image path within ClubConnect Directory for this method.
     public void setPhotoPath(String path) {
@@ -65,3 +69,4 @@ public class ProfilePhoto {
     }
 
 }
+//@author
