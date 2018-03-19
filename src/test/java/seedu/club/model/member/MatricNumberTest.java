0909814f4a354caf54586b0d98f9978695e924a6
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-19 18:22:21 +0800

    Added tests

--- src/test/java/seedu/club/model/member/MatricNumberTest.java
+++ src/test/java/seedu/club/model/member/MatricNumberTest.java
@@ -1,7 +1,9 @@
 package seedu.club.model.member;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.club.logic.commands.CommandTestUtil.VALID_MATRIC_NUMBER;
 
 import org.junit.Test;
 
@@ -43,5 +45,34 @@ public class MatricNumberTest {
         assertTrue(MatricNumber.isValidMatricNumber("A1902205L"));
         assertTrue(MatricNumber.isValidMatricNumber("a1024509A"));
         assertTrue(MatricNumber.isValidMatricNumber("a1122206M"));
+    }
+
+    @Test
+    public void test_toString() {
+        MatricNumber testMatricNumberOne = new MatricNumber("A0162345Y");
+        MatricNumber testMatricNumberTwo = new MatricNumber("A0189898D");
+
+        assertTrue(testMatricNumberOne.toString().equals("A0162345Y"));
+        assertFalse(testMatricNumberOne.toString().equals("A0189898D"));
+        assertTrue(testMatricNumberTwo.toString().equals("A0189898D"));
+        assertFalse(testMatricNumberTwo.toString().equals("A0162345Y"));
+    }
+
+    @Test
+    public void test_hashCode() {
+        MatricNumber testMatricNumber = new MatricNumber("A1234567M");
+        String matricNumber = "A1234567M";
+        assertEquals(matricNumber.hashCode(), testMatricNumber.hashCode());
+    }
+
+    @Test
+    public void test_equals() {
+        MatricNumber firstMatricNumber = new MatricNumber(VALID_MATRIC_NUMBER);
+        MatricNumber secondMatricNumber = new MatricNumber(VALID_MATRIC_NUMBER);
+
+        assertTrue(firstMatricNumber.equals(firstMatricNumber));
+        assertTrue(firstMatricNumber.equals(secondMatricNumber));
+
+        assertFalse(firstMatricNumber.equals(null));
     }
 }
