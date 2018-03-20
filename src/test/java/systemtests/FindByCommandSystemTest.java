43fc9ff69dbc4fe6a4172a8d2045343f9eb867a1
MuhdNurKamal <mnkamal95@gmail.com>
2018-03-20 19:11:33 +0800

    Add FindByCommandSystemTest

---
+++ src/test/java/systemtests/FindByCommandSystemTest.java
@@ -0,0 +1,203 @@
+package systemtests;
+
+import static org.junit.Assert.assertFalse;
+import static seedu.club.commons.core.Messages.MESSAGE_MEMBERS_LISTED_OVERVIEW;
+import static seedu.club.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.club.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.club.logic.parser.CliSyntax.PREFIX_MATRIC_NUMBER;
+import static seedu.club.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.club.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.club.testutil.TypicalMembers.BENSON;
+import static seedu.club.testutil.TypicalMembers.CARL;
+import static seedu.club.testutil.TypicalMembers.DANIEL;
+import static seedu.club.testutil.TypicalMembers.KEYWORD_MATCHING_MEIER;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import seedu.club.commons.core.index.Index;
+import seedu.club.logic.commands.DeleteCommand;
+import seedu.club.logic.commands.FindByCommand;
+import seedu.club.logic.commands.RedoCommand;
+import seedu.club.logic.commands.UndoCommand;
+import seedu.club.model.Model;
+import seedu.club.model.tag.Tag;
+
+public class FindByCommandSystemTest extends ClubBookSystemTest {
+
+    @Test
+    public void find() {
+        /* Case: find multiple members in club book, command with leading spaces and trailing spaces
+         * -> 2 members found
+         */
+        String command = "   " + FindByCommand.COMMAND_WORD + " " + KEYWORD_MATCHING_MEIER + "   ";
+        Model expectedModel = getModel();
+        ModelHelper.setFilteredList(expectedModel, BENSON, DANIEL); // first names of Benson and Daniel are "Meier"
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: repeat previous find command where member list is displaying the members we are finding
+         * -> 2 members found
+         */
+        command = FindByCommand.COMMAND_WORD + " " + KEYWORD_MATCHING_MEIER;
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find member where member list is not displaying the member we are finding -> 1 member found */
+        command = FindByCommand.COMMAND_WORD + " Carl";
+        ModelHelper.setFilteredList(expectedModel, CARL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find multiple members in club book, 2 keywords -> 2 members found */
+        command = FindByCommand.COMMAND_WORD + " Benson Daniel";
+        ModelHelper.setFilteredList(expectedModel, BENSON, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find multiple members in club book, 2 keywords in reversed order -> 2 members found */
+        command = FindByCommand.COMMAND_WORD + " Daniel Benson";
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find multiple members in club book, 2 keywords with 1 repeat -> 2 members found */
+        command = FindByCommand.COMMAND_WORD + " Daniel Benson Daniel";
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find multiple members in club book, 2 matching keywords and 1 non-matching keyword
+         * -> 2 members found
+         */
+        command = FindByCommand.COMMAND_WORD + " Daniel Benson NonMatchingKeyWord";
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: undo previous find command -> rejected */
+        command = UndoCommand.COMMAND_WORD;
+        String expectedResultMessage = UndoCommand.MESSAGE_FAILURE;
+        assertCommandFailure(command, expectedResultMessage);
+
+        /* Case: redo previous find command -> rejected */
+        command = RedoCommand.COMMAND_WORD;
+        expectedResultMessage = RedoCommand.MESSAGE_FAILURE;
+        assertCommandFailure(command, expectedResultMessage);
+
+        /* Case: find same members in club book after deleting 1 of them -> 1 member found */
+        executeCommand(DeleteCommand.COMMAND_WORD + " 1");
+        assertFalse(getModel().getClubBook().getMemberList().contains(BENSON));
+        command = FindByCommand.COMMAND_WORD + " " + KEYWORD_MATCHING_MEIER;
+        expectedModel = getModel();
+        ModelHelper.setFilteredList(expectedModel, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find member in club book, keyword is same as name but of different case -> 1 member found */
+        command = FindByCommand.COMMAND_WORD + " MeIeR";
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find member in club book, keyword is substring of name -> 2 members found */
+        command = FindByCommand.COMMAND_WORD + " Mei";
+        ModelHelper.setFilteredList(expectedModel, BENSON, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find member in club book, name is substring of keyword -> 0 members found */
+        command = FindByCommand.COMMAND_WORD + " Meiers";
+        ModelHelper.setFilteredList(expectedModel);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find member not in club book -> 0 members found */
+        command = FindByCommand.COMMAND_WORD + " " + PREFIX_NAME + " Mark";
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find phone number of member in club book -> 1 members found */
+        command = FindByCommand.COMMAND_WORD + " " + PREFIX_PHONE + " " + DANIEL.getPhone().value;
+        ModelHelper.setFilteredList(expectedModel, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find matric number of member in club book -> 1 members found */
+        command = FindByCommand.COMMAND_WORD + " " + PREFIX_MATRIC_NUMBER + " " + DANIEL.getMatricNumber().value;
+        ModelHelper.setFilteredList(expectedModel, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find email of member in club book -> 1 members found */
+        command = FindByCommand.COMMAND_WORD + " " + PREFIX_EMAIL + " " + BENSON.getEmail().value;
+        ModelHelper.setFilteredList(expectedModel, BENSON);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find tags of member in club book -> 0 members found */
+        List<Tag> tags = new ArrayList<>(BENSON.getTags());
+        command = FindByCommand.COMMAND_WORD + " " + tags.get(0).tagName;
+        ModelHelper.setFilteredList(expectedModel, BENSON);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: find while a member is selected -> selected card deselected */
+        showAllMembers();
+        selectMember(Index.fromOneBased(1));
+        assertFalse(getMemberListPanel().getHandleToSelectedCard().getName().equals(DANIEL.getName().fullName));
+        command = FindByCommand.COMMAND_WORD + " Daniel";
+        ModelHelper.setFilteredList(expectedModel, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardDeselected();
+
+        /* Case: find member in empty club book -> 0 members found */
+        deleteAllMembers();
+        command = FindByCommand.COMMAND_WORD + " " + KEYWORD_MATCHING_MEIER;
+        expectedModel = getModel();
+        ModelHelper.setFilteredList(expectedModel, DANIEL);
+        assertCommandSuccess(command, expectedModel);
+        assertSelectedCardUnchanged();
+
+        /* Case: mixed case command word -> rejected */
+        command = "FiNd Meier";
+        assertCommandFailure(command, MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    /**
+     * Executes {@code command} and verifies that the command box displays an empty string, the result display
+     * box displays {@code Messages#MESSAGE_MEMBERS_LISTED_OVERVIEW} with the number of people in the filtered list,
+     * and the model related components equal to {@code expectedModel}.
+     * These verifications are done by
+     * {@code ClubBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the status bar remains unchanged, and the command box has the default style class, and the
+     * selected card updated accordingly, depending on {@code cardStatus}.
+     * @see ClubBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandSuccess(String command, Model expectedModel) {
+        String expectedResultMessage = String.format(
+                MESSAGE_MEMBERS_LISTED_OVERVIEW, expectedModel.getFilteredMemberList().size());
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchanged();
+    }
+
+    /**
+     * Executes {@code command} and verifies that the command box displays {@code command}, the result display
+     * box displays {@code expectedResultMessage} and the model related components equal to the current model.
+     * These verifications are done by
+     * {@code ClubBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * Also verifies that the browser url, selected card and status bar remain unchanged, and the command box has the
+     * error style.
+     * @see ClubBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandFailure(String command, String expectedResultMessage) {
+        Model expectedModel = getModel();
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsErrorStyle();
+        assertStatusBarUnchanged();
+    }
+}
