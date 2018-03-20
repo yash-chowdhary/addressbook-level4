85bfe40827be3c2bd3107480ede095637c7fb602
amrut-prabhu <amrut08@gmail.com>
2018-03-21 02:30:35 +0800

    Update MemberCard (Tests work)

--- src/main/java/seedu/club/ui/MemberCard.java
+++ src/main/java/seedu/club/ui/MemberCard.java
@@ -1,11 +1,17 @@
 package seedu.club.ui;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import seedu.club.model.member.Member;
+import seedu.club.storage.ProfilePhotoStorage;
 
 /**
  * An UI component that displays information of a {@code member}.
@@ -13,6 +19,10 @@ import seedu.club.model.member.Member;
 public class MemberCard extends UiPart<Region> {
 
     private static final String FXML = "MemberListCard.fxml";
+    private static final Integer PHOTO_WIDTH = 100;
+    private static final Integer PHOTO_HEIGHT = 100;
+    private static final String DEFAULT_PHOTO = "src/main/resources/images/defaultProfilePhoto.png";
+    private static final String EMPTY_STRING = "";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -40,6 +50,8 @@ public class MemberCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    @FXML
+    private Circle profilePhoto;
 
     public MemberCard(Member member, int displayedIndex) {
         super(FXML);
@@ -62,6 +74,32 @@ public class MemberCard extends UiPart<Region> {
         group.setText(member.getGroup().groupName);
         email.setText(member.getEmail().value);
     }
+
+    //@@author amrut-prabhu
+    /**
+     * Sets the profile photo to the displayed photo shape.
+     */
+    private void setProfilePhoto(Member member) {
+        Image photo = null;
+        String photoPath;
+
+        if (!member.getProfilePhoto().getProfilePhotoPath().equals(EMPTY_STRING)) {
+            photoPath = member.getProfilePhoto().getProfilePhotoPath();
+
+            //Defensive programming
+            File file = new File(member.getProfilePhoto().getProfilePhotoPath());
+            if (!file.exists()) {
+                photoPath = ProfilePhotoStorage.getCurrentDirectory() + DEFAULT_PHOTO;
+            }
+        } else {
+            photoPath = ProfilePhotoStorage.getCurrentDirectory() + DEFAULT_PHOTO;
+        }
+
+        photo = new Image("file:" + photoPath, PHOTO_WIDTH, PHOTO_HEIGHT, false, false);
+
+        profilePhoto.setFill(new ImagePattern(photo));
+    }
+    //@@author
 
     @Override
     public boolean equals(Object other) {
