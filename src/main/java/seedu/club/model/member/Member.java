763db1e618d9bf31949940280c1fdaf3659104c3
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-24 18:05:12 +0800

    Update Member methods

--- src/main/java/seedu/club/model/member/Member.java
+++ src/main/java/seedu/club/model/member/Member.java
@@ -2,7 +2,6 @@ package seedu.club.model.member;
 
 import static seedu.club.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,7 +12,7 @@ import java.util.Set;
 import seedu.club.model.group.Group;
 import seedu.club.model.tag.Tag;
 import seedu.club.model.task.Task;
-import seedu.club.model.task.exceptions.DuplicateTaskException;
+import seedu.club.model.task.UniqueTaskList;
 
 /**
  * Represents a member in the club book.
@@ -31,8 +30,7 @@ public class Member {
     private Group group;
     private final HashMap<String, Tag> tags;
     private ProfilePhoto profilePhoto;
-    private ArrayList<Task> taskList;
-
+    private final UniqueTaskList tasks;
     /**
      * Every field must be present and not null.
      */
@@ -51,7 +49,24 @@ public class Member {
         this.password = password;
         this.profilePhoto = new ProfilePhoto("");
         setTags(tags);
-        this.taskList = null;
+        this.tasks = new UniqueTaskList();
+    }
+
+    public Member(Name name, Phone phone, Email email, MatricNumber matricNumber, Group group, Set<Tag> tags,
+                  Username username, Password password, Set<Task> tasks) {
+        requireAllNonNull(name, phone, email, matricNumber, group, tags,
+                username, password, tasks);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.matricNumber = matricNumber;
+        this.group = group;
+        this.tags = new HashMap<>();
+        this.username = username;
+        this.password = password;
+        this.profilePhoto = new ProfilePhoto("");
+        setTags(tags);
+        this.tasks = new UniqueTaskList(tasks);
     }
 
     //@@author amrut-prabhu
@@ -70,7 +85,7 @@ public class Member {
         this.username = member.username;
         this.password = member.password;
         this.profilePhoto = member.profilePhoto;
-        this.taskList = member.taskList;
+        this.tasks = member.tasks;
     }
     //@@author
 
@@ -145,6 +160,14 @@ public class Member {
         return Collections.unmodifiableSet(memberTags);
     }
 
+    /**
+     * Returns an immutable task set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Task> getTasks() {
+        return Collections.unmodifiableSet(tasks.toSet());
+    }
+
     public boolean hasTag(Tag tag) {
         return getTags().contains(tag);
     }
@@ -186,6 +209,8 @@ public class Member {
                 .append(getGroup())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
+        builder.append("Tasks: ");
+        getTasks().forEach(builder::append);
         return builder.toString();
     }
 
@@ -201,14 +226,4 @@ public class Member {
         }
     }
 
-    /**
-     * Adds {@code Task toAdd} to the taskList.
-     * @throws DuplicateTaskException if the task {@code toAdd} already exists.
-     */
-    public void addToTaskList(Task toAdd) throws DuplicateTaskException {
-        if (taskList.contains(toAdd)) {
-            throw new DuplicateTaskException();
-        }
-        taskList.add(toAdd);
-    }
 }
