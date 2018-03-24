b028869bdeaeef020ab9247e2ed09364b8407674
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-24 20:03:36 +0800

    Add TaskCard, TaskListPanel

--- src/main/java/seedu/club/commons/events/ui/TaskPanelSelectionChangedEvent.java
+++ src/main/java/seedu/club/commons/events/ui/TaskPanelSelectionChangedEvent.java
@@ -1,10 +1,14 @@
 package seedu.club.commons.events.ui;
 
 //@@author yash-chowdhary
+
+import seedu.club.commons.events.BaseEvent;
+import seedu.club.ui.TaskCard;
+
 /**
  * Represents a change in the Task List Panel
  */
-public class TaskPanelSelectionChangedEvent {
+public class TaskPanelSelectionChangedEvent extends BaseEvent {
     private final TaskCard newSelection;
 
     public TaskPanelSelectionChangedEvent(TaskCard newSelection) {
---
+++ src/main/java/seedu/club/ui/TaskListPanel.java
@@ -0,0 +1,91 @@
+package seedu.club.ui;
+
+import java.util.logging.Logger;
+
+import org.fxmisc.easybind.EasyBind;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.club.commons.core.LogsCenter;
+import seedu.club.commons.events.ui.JumpToListRequestEvent;
+import seedu.club.commons.events.ui.TaskPanelSelectionChangedEvent;
+import seedu.club.model.task.Task;
+
+/**
+ * Panel containing tasks to be completed by the members.
+ */
+public class TaskListPanel extends UiPart<Region> {
+    private static final String FXML = "TaskListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+
+    @FXML
+    private ListView<TaskCard> taskListView;
+
+    public TaskListPanel(ObservableList<Task> taskList) {
+        super(FXML);
+        setConnections(taskList);
+        registerAsAnEventHandler(this);
+    }
+
+    public void setConnections(ObservableList<Task> taskList) {
+        setMemberListView(taskList);
+        setEventHandlerForSelectionChangeEvent();
+    }
+
+    private void setEventHandlerForSelectionChangeEvent() {
+        taskListView.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        logger.fine("Selection in task list panel changed to : '" + newValue + "'");
+                        raise(new TaskPanelSelectionChangedEvent(newValue));
+                    }
+                });
+    }
+
+    public void setMemberListView(ObservableList<Task> taskList) {
+        ObservableList<TaskCard> mappedList = EasyBind.map(
+                taskList, (task) -> new TaskCard(task, taskList.indexOf(task) + 1));
+        taskListView.setItems(mappedList);
+        taskListView.setCellFactory(listView -> new TaskListViewCell());
+    }
+
+    /**
+     * Scrolls to the {@code TaskCard} at {@code index} and selects it.
+     */
+    private void scrollTo(int index) {
+        Platform.runLater(() -> {
+            taskListView.scrollTo(index);
+            taskListView.getSelectionModel().clearAndSelect(index);
+        });
+    }
+
+    @Subscribe
+    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        scrollTo(event.targetIndex);
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code TaskCard}.
+     */
+    class TaskListViewCell extends ListCell<TaskCard> {
+
+        @Override
+        protected void updateItem(TaskCard item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(item.getRoot());
+            }
+        }
+    }
+}
