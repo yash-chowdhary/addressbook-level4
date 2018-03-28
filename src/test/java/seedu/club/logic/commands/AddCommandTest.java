3aeebf1646dcecdfe682825912a7ae0912d7628b
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-28 11:53:43 +0800

    Fixes

--- src/test/java/seedu/club/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/club/logic/commands/AddCommandTest.java
@@ -219,6 +219,12 @@ public class AddCommandTest {
             fail("This method should not be called");
             return null;
         }
+
+        @Override
+        public void updateFilteredTaskList(Predicate<Task> predicate) {
+            fail("This method should not be called");
+            return;
+        }
     }
 
     /**
