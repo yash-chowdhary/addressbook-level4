211fd79c981886f661dd4cbf68af6800e30e0e12
yash-chowdhary <yashchowdhary98@gmail.com>
2018-03-29 22:01:16 +0800

    Remove select EventSubscriber in TaskListPanel

--- src/main/java/seedu/club/ui/TaskListPanel.java
+++ src/main/java/seedu/club/ui/TaskListPanel.java
@@ -66,14 +66,6 @@ public class TaskListPanel extends UiPart<Region> {
         });
     }
 
-    /*
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        scrollTo(event.targetIndex);
-    }
-    */
-
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code TaskCard}.
      */
