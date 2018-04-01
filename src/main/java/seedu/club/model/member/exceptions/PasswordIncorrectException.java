6eef94e87a2f40bcf7288658e5a93c708bd0ba0d
Song Weiyang <e0014576@u.nus.edu>
2018-04-01 22:27:49 +0800

    Add Password incorrect exception

---
+++ src/main/java/seedu/club/model/member/exceptions/PasswordIncorrectException.java
@@ -0,0 +1,7 @@
+package seedu.club.model.member.exceptions;
+
+/**
+ * Signals that the operations will result in an incorrect password entered
+ */
+public class PasswordIncorrectException extends Exception{
+}
