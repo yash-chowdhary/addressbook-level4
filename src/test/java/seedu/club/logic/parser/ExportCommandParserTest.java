cfaa67d49f89f2f00a1b24eb4d04e933bb9ad181
amrut-prabhu <amrut08@gmail.com>
2018-04-02 00:06:00 +0800

    Update test

--- src/test/java/seedu/club/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/club/logic/parser/ExportCommandParserTest.java
@@ -7,27 +7,34 @@ import static seedu.club.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.io.File;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import seedu.club.logic.commands.ExportCommand;
 
 public class ExportCommandParserTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     private ExportCommandParser parser = new ExportCommandParser();
     private String currentDirectoryPath = ".";
     private File currentDirectory = new File(currentDirectoryPath);
 
     @Test
-    public void parse_validArgs_returnsExportCommand() {
-        String expectedExportFilePath = currentDirectory.getAbsolutePath() + "/data/exportTestFile.csv";
+    public void parse_validArgs_returnsExportCommand() throws Exception {
+        File exportFile = temporaryFolder.newFile("actual.csv");
+
+        String expectedExportFilePath = exportFile.getAbsolutePath();
         File expectedExportFile = new File(expectedExportFilePath);
-        assertParseSuccess(parser, expectedExportFilePath, new ExportCommand(expectedExportFile));
+        assertParseSuccess(parser, expectedExportFile.getAbsolutePath(), new ExportCommand(expectedExportFile));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         //non absolute file path
-        assertParseFailure(parser, "/data/exportTestFile.csv", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        assertParseFailure(parser, "data/exportTestFile.csv", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 ExportCommand.MESSAGE_USAGE));
 
         //invalid file path
