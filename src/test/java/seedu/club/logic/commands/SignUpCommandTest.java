b07eb19f2aac8789d27072e46202d651d68b474a
yash-chowdhary <yashchowdhary98@gmail.com>
2018-04-02 01:03:01 +0800

    Fixes

--- src/test/java/seedu/club/logic/commands/SignUpCommandTest.java
+++ src/test/java/seedu/club/logic/commands/SignUpCommandTest.java
@@ -30,6 +30,7 @@ import seedu.club.model.task.Task;
 import seedu.club.model.task.exceptions.DuplicateTaskException;
 import seedu.club.model.task.exceptions.TaskCannotBeDeletedException;
 import seedu.club.model.task.exceptions.TaskNotFoundException;
+import seedu.club.model.task.exceptions.TasksAlreadyListedException;
 import seedu.club.model.task.exceptions.TasksCannotBeDisplayedException;
 import seedu.club.testutil.MemberBuilder;
 
@@ -75,6 +76,11 @@ public class SignUpCommandTest {
         @Override
         public void addMember(Member member) throws DuplicateMemberException {
             fail("This method should not be called.");
+        }
+
+        @Override
+        public void viewMyTasks() throws TasksAlreadyListedException {
+            fail("This method should not be called");
         }
 
         @Override
