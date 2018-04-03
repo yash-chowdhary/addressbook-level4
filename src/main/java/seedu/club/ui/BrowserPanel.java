20be93a52c34f38c8079b293aa7d7ca297a22c62
Song Weiyang <e0014576@u.nus.edu>
2018-04-03 13:43:56 +0800

    Add MemberDetailsPanel

--- src/main/java/seedu/club/ui/BrowserPanel.java
+++ src/main/java/seedu/club/ui/BrowserPanel.java
@@ -2,6 +2,7 @@ package seedu.club.ui;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
 import java.util.logging.Logger;
@@ -11,6 +12,10 @@ import com.google.common.eventbus.Subscribe;
 import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import seedu.club.MainApp;
@@ -24,29 +29,46 @@ import seedu.club.model.member.Member;
  * The Browser Panel of the App.
  */
 public class BrowserPanel extends UiPart<Region> {
-
-    public static final String DEFAULT_PAGE = "default.html";
-    public static final String SEARCH_PAGE_URL =
-            "https://se-edu.github.io/addressbook-level4/DummySearchPage.html?name=";
     public static final String GMAIL_URL = "https://mail.google.com/mail/?view=cm&fs=1&to=%1$s"
             + "&su=%2$s&body=%3$s";
     public static final String OUTLOOK_URL = "https://outlook.office.com/?path=/mail/action/"
             + "compose&to=%1$s&subject=%2$s&body=%3$s";
 
-    private static final String FXML = "BrowserPanel.fxml";
+    private static final String FXML = "MemberDetailsPanel.fxml";
+
+    private static final Integer PHOTO_WIDTH = 90;
+    private static final Integer PHOTO_HEIGHT = 120;
+    private static final String DEFAULT_PHOTO = "/images/defaultProfilePhoto.png";
+    private static final String EMPTY_STRING = "";
+    private static final String[] TAG_COLORS = {"red", "yellow", "grey", "brown", "pink", "white",
+            "orange", "blue", "violet"};
 
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     @FXML
     private WebView browser;
+    @FXML
+    private Label name;
+    @FXML
+    private ImageView profilePhoto;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label matricNumber;
+    @FXML
+    private Label groups;
+    @FXML
+    private Label email;
+    @FXML
+    private FlowPane tags;
+
+
 
     public BrowserPanel() {
         super(FXML);
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
-
-        loadDefaultPage();
         registerAsAnEventHandler(this);
     }
 
@@ -102,20 +124,15 @@ public class BrowserPanel extends UiPart<Region> {
     //@@author
 
     private void loadMemberPage(Member member) {
-        loadPage(SEARCH_PAGE_URL + member.getName().fullName);
+        name.setText(member.getName().fullName);
+        setProfilePhoto(member);
+        phone.setText(member.getPhone().value);
+        matricNumber.setText(member.getMatricNumber().value);
+        email.setText(member.getEmail().value);
+        groups.setText(member.getGroup().groupName);
+        createTags(member);
     }
 
-    public void loadPage(String url) {
-        Platform.runLater(() -> browser.getEngine().load(url));
-    }
-
-    /**
-     * Loads a default HTML file with a background that matches the general theme.
-     */
-    private void loadDefaultPage() {
-        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
-        loadPage(defaultPage.toExternalForm());
-    }
 
     /**
      * Frees resources allocated to the browser.
@@ -138,4 +155,47 @@ public class BrowserPanel extends UiPart<Region> {
         callClient(event.getClient().toString(), event.getRecipients(), event.getSubject().toString(),
                 event.getBody().toString());
     }
+
+    //@@author amrut-prabhu
+    /**
+     * Sets the profile photo to the displayed photo shape.
+     */
+    private void setProfilePhoto(Member member) {
+        Image photo;
+        String photoPath = member.getProfilePhoto().getProfilePhotoPath();
+        if (photoPath.equals(EMPTY_STRING)) {
+            photo = new Image(MainApp.class.getResourceAsStream(DEFAULT_PHOTO),
+                    PHOTO_WIDTH, PHOTO_HEIGHT, false, true);
+        } else {
+            try {
+                InputStream photoStream = MainApp.class.getResourceAsStream(photoPath);
+                photo = new Image("file:" + photoPath, PHOTO_WIDTH, PHOTO_HEIGHT, false, false);
+            } catch (NullPointerException npe) {
+                photo = new Image(MainApp.class.getResourceAsStream("/images/default.png"), //DEFAULT_PHOTO),
+                        PHOTO_WIDTH, PHOTO_HEIGHT, false, true);
+            }
+        }
+        profilePhoto.setImage(photo);
+    }
+    //@@author
+
+    //@@author yash-chowdhary
+    /**
+     * Creates the labels for tags by randomly generating a color from `TAG_COLORS`
+     */
+    private void createTags(Member member) {
+        member.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.getStyleClass().add(returnColor(tag.tagName));
+            tags.getChildren().add(tagLabel);
+        });
+    }
+
+    /**
+     * Returns a color chosen uniformly at random from TAG_COLORS
+     */
+    private String returnColor(String tag) {
+        return TAG_COLORS[Math.abs(tag.hashCode()) % TAG_COLORS.length];
+    }
+    //@@author
 }
