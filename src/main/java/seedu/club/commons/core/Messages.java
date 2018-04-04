ba421b750cb9591f1c5ec24fbd37cbc28675ba76
Song Weiyang <e0014576@u.nus.edu>
2018-04-04 10:06:07 +0800

    Add error messages

--- src/main/java/seedu/club/commons/core/Messages.java
+++ src/main/java/seedu/club/commons/core/Messages.java
@@ -13,5 +13,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_POLL_DISPLAYED_INDEX = "The poll index provided is invalid";
     public static final String MESSAGE_INVALID_PERMISSIONS = "This command cannot be executed as you don't have EXCO "
             + "privileges";
+    public static final String MESSAGE_REQUIRE_LOG_IN = "Please log in to Club Connect.";
+    public static final String MESSAGE_REQUIRE_SIGN_UP = "Please sign up to use Club Connect";
 
 }
