6c5cba2b10d2cc1cce68e688001620d8329e3418
Song Weiyang <e0014576@u.nus.edu>
2018-04-06 00:04:06 +0800

    Rename error message

--- src/main/java/seedu/club/commons/core/Messages.java
+++ src/main/java/seedu/club/commons/core/Messages.java
@@ -15,9 +15,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_ANSWER_DISPLAYED_INDEX = "The answer index provided is invalid";
     public static final String MESSAGE_INVALID_PHOTO_PATH = "Unable to read profile photo from the path: %1$s";
     public static final String MESSAGE_SAME_PHOTO_PATH = "Profile photo path entered is same as the current photo";
-    public static final String MESSAGE_INVALID_PERMISSIONS = "This command cannot be executed as you don't have EXCO "
-            + "privileges";
     public static final String MESSAGE_REQUIRE_LOG_IN = "Please log in to Club Connect.";
     public static final String MESSAGE_REQUIRE_SIGN_UP = "Please sign up to use Club Connect";
-    public static final String MESSAGE_REQUIRE_EXCO_LOG_IN = "Only excos are authorised to execute current command";
+    public static final String MESSAGE_REQUIRE_EXCO_LOG_IN = "You need to be an exco to use these features";
 }
