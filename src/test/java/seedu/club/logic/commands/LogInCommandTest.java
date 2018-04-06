b1e1d89162928b2a7a0b6095e4a821a17f2ec75a
Song Weiyang <e0014576@u.nus.edu>
2018-04-06 14:52:57 +0800

    Modfiy test cases

--- src/test/java/seedu/club/logic/commands/LogInCommandTest.java
+++ src/test/java/seedu/club/logic/commands/LogInCommandTest.java
@@ -329,7 +329,7 @@ public class LogInCommandTest {
 
         @Override
         public Member getLoggedInMember() {
-            return null;
+            return currentlyLoggedIn;
         }
         //@@author
     }
