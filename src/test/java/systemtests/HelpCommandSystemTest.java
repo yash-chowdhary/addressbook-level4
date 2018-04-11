dafc887ad4df701610107150441f3afc9c2e9cba
Song Weiyang <e0014576@u.nus.edu>
2018-04-11 10:31:59 +0800

    Reactify checkStyle

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -1,7 +1,6 @@
 package systemtests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.club.testutil.TypicalIndexes.INDEX_FIRST_MEMBER;
