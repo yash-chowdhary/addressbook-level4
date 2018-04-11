a6826f2116cd048a3eed78ebdcd150df56844f1c
Song Weiyang <e0014576@u.nus.edu>
2018-04-11 10:00:15 +0800

    Add indicate change panel event

--- src/main/java/seedu/club/commons/events/ui/ClearMemberSelectPanelEvent.java
+++ src/main/java/seedu/club/commons/events/ui/ClearMemberSelectPanelEvent.java
@@ -18,6 +18,6 @@ public class ClearMemberSelectPanelEvent extends BaseEvent {
 
     @Override
     public String toString() {
-        return null;
+        return this.getClass().getSimpleName();
     }
 }
